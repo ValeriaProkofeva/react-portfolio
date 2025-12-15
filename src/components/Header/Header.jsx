1de4bda1e938f876b110262e3c248607a29ce92a
Прокофьева Валерия Денисовна <prokofeva.vd2001@okeit.edu>
2025-12-15 16:47:26 +0500

    Add Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
-
+import styles from "./Header.module.css"
 
 function Header() {
 
   return (
     <>
+    <div className={styles.contNav}>
+    <div className={styles.name}>
     <p>Прокофьева.Исаев</p>
-    <div className="">
+    </div>
+    <div className={styles.nav}>
     <p>навигация</p>
     <p>навигация</p>
     <p>навигация</p>
     <p>навигация</p>
+    </div>
     </div>
     </>
   )
