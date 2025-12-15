82cfd9a39b3a7c66feb03d99d7073a69404b9ee4
Прокофьева Валерия Денисовна <prokofeva.vd2001@okeit.edu>
2025-12-15 16:42:08 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+
+
+function Header() {
+
+  return (
+    <>
+    <p>Прокофьева.Исаев</p>
+    <div className="">
+    <p>навигация</p>
+    <p>навигация</p>
+    <p>навигация</p>
+    <p>навигация</p>
+    </div>
+    </>
+  )
+}
+
+export default Header
